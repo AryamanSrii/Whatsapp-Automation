7b952ffba9d686e8f217fa9ae9db17b5223bf21c
Aryaman Srivastava <aryamansrivastava2006@gmail.com>
2021-02-15 20:35:22 +0530

    Create Whatsapp.py

---
+++ Whatsapp.py
@@ -0,0 +1,6 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('#phone number','#message',20,30) 
+                                                  ^
+                                                  |
+                                                  |
+                                          #example of time (use 24 hr clock)
